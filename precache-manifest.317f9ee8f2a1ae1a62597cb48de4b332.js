c89d706bc0900ccc378e96f148182422f970b0c3
Faiz Hameed <faizftb@gmail.com>
2019-06-07 11:47:40 +0530

    Updates

---
+++ precache-manifest.317f9ee8f2a1ae1a62597cb48de4b332.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64397104572363b4cbe7eb57cea9ff82",
+    "url": "/emailChips/index.html"
+  },
+  {
+    "revision": "a87e4cd79017fe1d48c2",
+    "url": "/emailChips/static/css/main.59a4c368.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/emailChips/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "a87e4cd79017fe1d48c2",
+    "url": "/emailChips/static/js/main.dcf72a6a.chunk.js"
+  },
+  {
+    "revision": "da044cb3de44fd75ffa7",
+    "url": "/emailChips/static/js/runtime~main.85a70f2e.js"
+  }
+]);
